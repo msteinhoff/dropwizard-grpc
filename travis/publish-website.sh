248585dd57ae1cc70dd3d0226177b0c5993f7744
Mario Steinhoff <steinhoff.mario@gmail.com>
2017-01-06 21:39:06 +0100

    Problem: Javadoc should be updated only for non-snapshot releases
    Solution: Rework publish-websites.sh script

--- travis/publish-website.sh
+++ travis/publish-website.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+STAGING_DIRECTORY=$HOME/ghp-staging
 REPO_SLUG=msteinhoff/dropwizard-grpc
 BRANCH=master
 JDK_VERSION=oraclejdk8
@@ -9,35 +10,51 @@ JDK_VERSION=oraclejdk8
 [ "$TRAVIS_BRANCH" == "$BRANCH" ] || { echo "Skipping build, expected TRAVIS_BRANCH to be $BRANCH but was $TRAVIS_BRANCH"; exit; }
 [ "$TRAVIS_JDK_VERSION" == "$JDK_VERSION" ] || { echo "Skipping build, expected TRAVIS_JDK_VERSION to be $JDK_VERSION but was $TRAVIS_JDK_VERSION"; exit; }
 
-echo -e "Publishing website to gh-pages.\n"
-mkdir -p $HOME/ghp-staging
+. version.properties
 
-echo -e "Building javadoc...\n"
-./gradlew javadoc
+[ -z "$DROPWIZARD_VERSION" ] && { "Skipping build because DROPWIZARD_VERSION not set"; exit; }
+[ -z "$PROJECT_VERSION" ] && { "Skipping build because PROJECT_VERSION not set"; exit; }
+[ -z "$SNAPSHOT" ] && { "Skipping build because SNAPSHOT not set"; exit; }
 
 echo -e "Publishing README and CHANGELOG...\n"
-cp _config.yml $HOME/ghp-staging
-cp README.md $HOME/ghp-staging
-cp CHANGELOG.md $HOME/ghp-staging
+mkdir -p $STAGING_DIRECTORY
+cp _config.yml $STAGING_DIRECTORY
+cp README.md $STAGING_DIRECTORY
+cp CHANGELOG.md $STAGING_DIRECTORY
 
-echo -e "Publishing reports and javadoc...\n"
-cp -R build/reports/findbugs $HOME/ghp-staging/findbugs
-cp -R build/reports/pmd $HOME/ghp-staging/pmd
-cp -R build/reports/tests/test $HOME/ghp-staging/tests
-cp -R build/reports/jacoco $HOME/ghp-staging/jacoco
-cp -R build/docs/javadoc $HOME/ghp-staging/javadoc
+if [ "$SNAPSHOT" = "false" ];
+then
+    RELEASE_VERSION="$DROPWIZARD_VERSION-$PROJECT_VERSION"
+    echo -e "Building javadoc...\n"
+    ./gradlew javadoc
+
+    echo -e "Staging findbugs, pmd, junit, jacodo reports and javadoc...\n"
+    mkdir -p $STAGING_DIRECTORY/$RELEASE_VERSION
+    cp -R build/reports/findbugs $STAGING_DIRECTORY/$RELEASE_VERSION/findbugs
+    cp -R build/reports/pmd $STAGING_DIRECTORY/$RELEASE_VERSION/pmd
+    cp -R build/reports/tests/test $STAGING_DIRECTORY/$RELEASE_VERSION/tests
+    cp -R build/reports/jacoco $STAGING_DIRECTORY/$RELEASE_VERSION/jacoco
+    cp -R build/docs/javadoc $STAGING_DIRECTORY/$RELEASE_VERSION/javadoc
+fi
 
 echo -e "Cloning gh-pages branch...\n"
 cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/$REPO_SLUG.git ghp-repo > /dev/null
+git clone --quiet --branch=gh-pages https://$GH_TOKEN@github.com/$REPO_SLUG.git ghp-repo > /dev/null
 
 echo -e "Updating gh-pages branch...\n"
 cd ghp-repo
-git rm -rf ./*
-cp -Rf $HOME/ghp-staging/* .
-git add -f .
+git rm -f ./*
+cp -f $STAGING_DIRECTORY/* .
+
+if [ "$SNAPSHOT" = "false" ];
+then
+    mkdir $RELEASE_VERSION/
+    cp -Rf $STAGING_DIRECTORY/$RELEASE_VERSION/* $RELEASE_VERSION/
+fi
+
+git add -A -f .
 git commit -m "Latest page content on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
 
